c531e0a23586b7118672b913d5f9be5b2f24c011
Andreyeyeye <shebanoff.andrei@yandex.ru>
2025-02-03 21:25:01 +0700

    Добавьте файлы проекта.

---
+++ GPILabs/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GPILabs
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		string lab1path = "";
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void l1OpenFile_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+
+
+			if (openFileDialog.ShowDialog() == true)
+			{
+				lab1path = openFileDialog.FileName;
+			}
+			Console.WriteLine(lab1path);
+		}
+
+		private void l1Edit_Click(object sender, RoutedEventArgs e)
+		{
+			List<byte> data = l1.GetBytesFromBMP(lab1path);
+			List<byte> result = l1.RGBToBW(data);
+
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			if (saveFileDialog.ShowDialog() == true)
+			{
+				lab1path = saveFileDialog.FileName;
+			}
+			Console.WriteLine(lab1path);
+			l1.SetBytesToBMP(lab1path, result);
+		
+		}
+
+	}
+}
---
+++ GPILabs/l1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPILabs
+{
+	internal class l1
+	{
+
+		public static List<byte> RGBToBW(List<byte> data)
+		{
+			List<byte> result = new List<byte>(data);
+			int ColorsCount = BitConverter.ToInt32(data.GetRange(46, 50).ToArray(),0);
+			Console.WriteLine(ColorsCount);
+
+			for(int i = 0; i<ColorsCount; i++)
+			{
+				int R = data[54 + (i * 4)];
+				int G = data[55 + (i * 4)];
+				int B = data[56 + (i * 4)];
+				
+				//int NewColor = ((int)((R * 0.3) + (G * 0.59) + (B * 0.11)))/3; мне не понравилось затемнение
+				int NewColor = ((int)(R + G + B))/3;
+				result[54 + (i * 4)] = (byte)NewColor;
+				result[55 + (i * 4)] = (byte)NewColor;
+				result[56 + (i * 4)] = (byte)NewColor;
+			}
+
+			return result;
+		}
+
+
+
+
+
+
+		public static List<byte> GetBytesFromBMP(string path)
+		{
+			List<byte> bytes = new List<byte>();
+
+			try
+			{
+				bytes = File.ReadAllBytes(path).ToList();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Ошибка при чтении файла: " + ex.Message);
+				return null;
+			}
+
+
+			return bytes;
+		}
+
+		public static void SetBytesToBMP(string path, List<byte> bytes)
+		{
+			try
+			{
+				// Записываем массив байтов в файл
+				File.WriteAllBytes(path, bytes.ToArray());
+				Console.WriteLine("Файл успешно записан.");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Ошибка при записи файла: {ex.Message}");
+			}
+		}
+
+
+	}
+}
